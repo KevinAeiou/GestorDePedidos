a50c6bcc647f990ef53b3b2b9db0033d6a4a5d4b
Kevin <kevin.souzaeiou@outlook.com>
2023-12-12 20:17:28 -0400

    Atualização no layout.

--- app/src/main/java/com/example/toyapedidos/ui/cardapio/CardapioViewModel.java
+++ app/src/main/java/com/example/toyapedidos/ui/cardapio/CardapioViewModel.java
@@ -1,16 +1,16 @@
-package com.example.toyapedidos.ui.home;
+package com.example.toyapedidos.ui.cardapio;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class CardapioViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public CardapioViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is gallery fragment");
     }
 
     public LiveData<String> getText() {
--- app/src/main/java/com/example/toyapedidos/ui/pedidos/PedidosViewModel.java
+++ app/src/main/java/com/example/toyapedidos/ui/pedidos/PedidosViewModel.java
@@ -1,16 +1,16 @@
-package com.example.toyapedidos.ui.slideshow;
+package com.example.toyapedidos.ui.pedidos;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class PedidosViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public PedidosViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
