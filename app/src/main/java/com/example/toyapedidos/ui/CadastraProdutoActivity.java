f0c7e7d696f8e60b71e925665715f083ca3bde9b
KevinAeiou <kevin.souzaeiou@outlook.com>
2023-12-14 10:59:39 -0400

    Implementação da activity_cadastra_produto.xml.

---
+++ app/src/main/java/com/example/toyapedidos/ui/CadastraProdutoActivity.java
@@ -0,0 +1,16 @@
+package com.example.toyapedidos.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.toyapedidos.R;
+
+public class CadastraProdutoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastra_produto);
+    }
+}
